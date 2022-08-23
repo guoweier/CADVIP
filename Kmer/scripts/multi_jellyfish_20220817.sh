98594dffa81cc89e00ce99c2fd6803f3bc86e3e7
Weier Guo <weguo@ucdavis.edu>
2022-08-23 11:04:28 -0700

    update kmer width

--- Kmer/scripts/multi_jellyfish_20220817.sh
+++ Kmer/scripts/multi_jellyfish_20220817.sh
@@ -4,11 +4,11 @@
 # Introduction
 # Multiple running of jellyfish count + jellyfish histo. 
 
-kmers="15 17 19 23 25 27 29"
+kmers="15 17 19 21 23 25 27 29 31"
 
 for kmer in $kmers
 do 
 	echo $kmer 
-	jellyfish count <(zcat width1.fq.gz) -C -m $kmer -t 10 -s 1G -o width1_${kmer}.jf
-	jellyfish histo -t 10 width1_${kmer}.jf > width1_${kmer}.histo
+	jellyfish count <(zcat width2.fq.gz) -C -m $kmer -t 10 -s 1G -o width2_${kmer}.jf
+	jellyfish histo -t 10 width2_${kmer}.jf > width2_${kmer}.histo
 done
