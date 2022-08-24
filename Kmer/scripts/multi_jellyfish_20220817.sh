ad1143377916389a8a746850ad852c3f909f8253
Weier Guo <weguo@ucdavis.edu>
2022-08-24 14:54:05 -0700

    Kmer depth

--- Kmer/scripts/multi_jellyfish_20220817.sh
+++ Kmer/scripts/multi_jellyfish_20220817.sh
@@ -9,6 +9,6 @@ kmers="15 17 19 21 23 25 27 29 31"
 for kmer in $kmers
 do 
 	echo $kmer 
-	jellyfish count <(zcat depth2.fq.gz) -C -m $kmer -t 10 -s 1G -o depth2_${kmer}.jf
-	jellyfish histo -t 10 depth2_${kmer}.jf > depth2_${kmer}.histo
+	jellyfish count <(zcat mvd1.fq.gz) -C -m $kmer -t 10 -s 1G -o mvd1_${kmer}.jf
+	jellyfish histo -t 10 mvd1_${kmer}.jf > mvd1_${kmer}.histo
 done
