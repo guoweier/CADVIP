30d40abc8c9bebd146f6e3b8d8ae3acd9804f74e
Weier Guo <weguo@ucdavis.edu>
2022-08-26 15:34:22 -0700

    jellyfish dump rerun

--- Kmer/scripts/jellyfish_count_dump_20220825.sh
+++ Kmer/scripts/jellyfish_count_dump_20220825.sh
@@ -15,15 +15,15 @@ do
 		for bulk in $bulks
 		do 
 			echo $bulk
-			jellyfish count <(zcat ${pheno}${bulk}.fq.gz) -C -m 21 -t 10 -s 1G -o ${pheno}${bulk}_21.jf
-			jellyfish dump -L 3 ${pheno}${bulk}_21.jf > ${pheno}${bulk}_21.fa
+			# jellyfish count <(zcat ${pheno}${bulk}.fq.gz) -C -m 21 -t 10 -s 1G -o ${pheno}${bulk}_21.jf
+			jellyfish dump -L 5 -c -t ${pheno}${bulk}_21.jf > ${pheno}${bulk}_21.fa
 		done
 	else
 		for bulk in $bulks
 		do 
 			echo $bulk
-			jellyfish count <(zcat ${pheno}${bulk}.fq.gz) -C -m 31 -t 10 -s 1G -o ${pheno}${bulk}_31.jf
-			jellyfish dump -L 3 ${pheno}${bulk}_31.jf > ${pheno}${bulk}_31.fa
+			# jellyfish count <(zcat ${pheno}${bulk}.fq.gz) -C -m 31 -t 10 -s 1G -o ${pheno}${bulk}_31.jf
+			jellyfish dump -L 5 -c -t ${pheno}${bulk}_31.jf > ${pheno}${bulk}_31.fa
 		done
 	fi
 done
